c762ae489772ff8fb2e612c1603f9ff5218339a9
John-Johns0n <84598043+John-Johns0n@users.noreply.github.com>
2024-12-02 13:17:04 -0500

    Initial commit

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
-from db_config import DB_HOST, DB_USER, DB_PASSWORD
+from fastapi import FastAPI
+from db_config import DB_NAME, DB_HOST, DB_USER, DB_PASSWORD
+
+
+DATABASE_URL = f'postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}/{DB_NAME}'
+
+
+app = FastAPI()
+
+
+@app.get('/')
+def read_root() -> dict[str, str]:
+    return {'message': 'API is running'}
+
+
