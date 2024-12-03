2b88d15daa8ab4deac7e3b0413f78d13a50b9f35
John-Johns0n <84598043+John-Johns0n@users.noreply.github.com>
2024-12-03 13:54:07 -0500

    Improve Docker setup

--- main.py
+++ main.py
@@ -1,15 +1,12 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Response
 from db_config import DB_NAME, DB_HOST, DB_USER, DB_PASSWORD
 
 
 DATABASE_URL = f'postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}/{DB_NAME}'
 
-
 app = FastAPI()
 
 
 @app.get('/')
-def read_root() -> dict[str, str]:
-    return {'message': 'API is running'}
-
-
+def read_root() -> Response:
+    return Response(status_code=200, content='API is running')
