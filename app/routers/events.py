da69f69c91d55af6afd29881d9e4aa766b9433a1
John-Johns0n <84598043+John-Johns0n@users.noreply.github.com>
2024-12-04 14:24:13 -0500

    Fix typo

--- app/routers/events.py
+++ app/routers/events.py
@@ -13,7 +13,7 @@ def get_events(
         event_type: Annotated[int | None, Query(gt=0)] = None, event_city: str | None = None,
         event_country: str | None = None, online: bool | None = None,
         minimum_player_count: Annotated[int, Query(gt=0)] = 1,
-        from_date: datetime = datetime.date(2000, 1, 1), until_date: datetime = datetime.date.today(),
+        from_date: datetime.date = datetime.date(2000, 1, 1), until_date: datetime.date = datetime.date.today(),
         page: Annotated[int, Query(gt=0)] = 1, per_page: Annotated[int, Query(gt=0)] = 100) -> Response:
     return Response(status_code=501, content='This route has not yet been implemented')
 
