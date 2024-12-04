1cd3c139c79fda665b32ddf67614571aae62715e
John-Johns0n <84598043+John-Johns0n@users.noreply.github.com>
2024-12-04 11:45:17 -0500

    Give optional params default value = None

--- app/routers/events.py
+++ app/routers/events.py
@@ -9,9 +9,10 @@ router = APIRouter(
 
 @router.get('')
 def get_events(
-        event_region: int | None, event_type: int | None, event_city: str | None, event_country: str | None,
-        online: bool | None, minimum_player_count: int = 1, from_date: datetime = datetime.date(2000, 1, 1),
-        until_date: datetime = datetime.date.today(), page: int = 1, per_page: int = 100) -> Response:
+        event_region: int | None = None, event_type: int | None = None, event_city: str | None = None,
+        event_country: str | None = None, online: bool | None = None, minimum_player_count: int = 1,
+        from_date: datetime = datetime.date(2000, 1, 1), until_date: datetime = datetime.date.today(),
+        page: int = 1, per_page: int = 100) -> Response:
     return Response(status_code=501, content='This route has not yet been implemented')
 
 
