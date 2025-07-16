08595601028d0d0b9c37380d930195ca2b5cc9fa
John-Johns0n <84598043+John-Johns0n@users.noreply.github.com>
2025-07-16 16:23:52 -0400

    Implement /players routes GET `/players` and GET `/players/{id}` done, GET `/players/{id}/event_results` WIP

--- app/models.py
+++ app/models.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Column, Integer, String, Numeric
 from app.db.database import Base, engine
 
 
@@ -8,6 +8,45 @@ class Club(Base):
     id = Column(Integer, primary_key=True, index=True)
     club_name = Column(String, index=True)
     club_short_name = Column(String)
+
+class Player(Base):
+    __tablename__ = 'players'
+
+    id = Column(Integer, primary_key=True, index=True)
+    first_name = Column(String)
+    last_name = Column(String)
+    player_region = Column(Integer)
+    player_club = Column(Integer)
+    player_score_2025_cycle = Column(Numeric)
+    player_score_2028_cycle = Column(Numeric)
+
+class EventResult(Base):
+    __tablename__ = 'event_results'
+
+    id = Column(Integer, primary_key=True, index=True)
+    event_id = Column(Integer)
+    player_id = Column(Integer)
+    player_first_name = Column(String)
+    player_last_name = Column(String)
+    placement = Column(Integer)
+    score = Column(Numeric)
+
+# class PlayerScores2025(Base):
+#     __tablename__ = 'player_scores_2025_cycle'
+#
+#     id = Column(Integer, primary_key=True, index=True)
+#     player_id = Column(Integer)
+#     total_score = Column(Numeric)
+#     out_of_region_live = Column(Integer)
+#     other_live_1 = Column(Integer)
+#     other_live_2 = Column(Integer)
+#     any_event_1 = Column(Integer)
+#     any_event_2 = Column(Integer)
+#     tank_1 = Column(Integer)
+#     tank_2 = Column(Integer)
+#     tank_3 = Column(Integer)
+#     tank_4 = Column(Integer)
+#     tank_5 = Column(Integer)
 
 
 def create_database():
--- app/routers/players.py
+++ app/routers/players.py
@@ -1,18 +1,110 @@
 from typing import Annotated
 
-from fastapi import APIRouter, Response, Query
+from fastapi import APIRouter, HTTPException, status, Depends, Query, Response
+from sqlalchemy import asc
+from sqlalchemy.orm import Session
+from pydantic import BaseModel
+
+from app.db.database import get_db
+from app.models import Player as PlayerModel
+from app.models import EventResult as ResultModel
+from app.authentication import validate_api_key
 
 router = APIRouter(
     prefix='/api/v1/players'
 )
 
+class Player(BaseModel):
+    id: int
+    first_name: str
+    last_name: str
+    player_region: int
+    player_club: int | None
+    player_score_2025_cycle: float | None
+    player_score_2028_cycle: float | None
+    event_results: str
+
+class Players(BaseModel):
+    records: list[Player]
+
+class Result(BaseModel):
+    event_name: str
+    placement: int
+    score: float | None
+
+class Results (BaseModel):
+    records: list[Result]
+
+# class PlayerScores2025(BaseModel):
+#     id: int
+#     player_id: int
+#     total_score: float | None
+#
+# class Scores2025(BaseModel):
+#     records: list[PlayerScores2025]
 
 @router.get('')
-def get_players(score_limit_2025: Annotated[float, Query(ge=0, le=6000)] = 0,
-                page: Annotated[int, Query(gt=0)] = 1, per_page: Annotated[int, Query(gt=0)] = 100) -> Response:
-    return Response(status_code=501, content='This route has not yet been implemented')
+async def get_players(
+    _: str = Depends(validate_api_key),
+    db: Session = Depends(get_db),
+) -> Players:
+    players = db.query(PlayerModel).order_by(asc(PlayerModel.id)).all()
+
+    data = Players(
+        records=[
+            Player(
+                id=player.id,
+                first_name=player.first_name,
+                last_name=player.last_name,
+                player_region=player.player_region,
+                player_club=player.player_club,
+                player_score_2025_cycle=player.player_score_2025_cycle,
+                player_score_2028_cycle=player.player_score_2028_cycle,
+                event_results=f'/players/{player.id}/event_results'
+            ) for player in players
+        ]
+    )
+
+    return data
 
 
 @router.get('/{player_id}')
-def get_player(player_id: int) -> Response:
-    return Response(status_code=501, content='This route has not yet been implemented')
+async def get_player(
+    player_id: int,
+    db: Session = Depends(get_db),
+    _: str = Depends(validate_api_key)
+) -> Player:
+    player = db.query(PlayerModel).filter(PlayerModel.id == player_id).first()
+    if player is None:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f'No player with id {player_id}')
+
+    data = Player(
+        id=player.id,
+        first_name=player.first_name,
+        last_name=player.last_name,
+        player_region=player.player_region,
+        player_club=player.player_club,
+        player_score_2025_cycle=player.player_score_2025_cycle,
+        player_score_2028_cycle=player.player_score_2028_cycle,
+        event_results=f'/players/{player_id}/event_results'
+    )
+
+    return data
+
+@router.get('/{player_id}/event_results')
+async def get_player_results(
+    player_id: int,
+    db: Session = Depends(get_db),
+    _: str = Depends(validate_api_key)
+) -> Results:
+    results = db.query(ResultModel).filter(ResultModel.player_id == player_id).all()
+
+    if results is None:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f'No results found for player with id {player_id}')
+
+    data = Results(
+        records=[
+            Result(event_name=str(result.event_id), placement=result.placement, score=result.score) for result in results
+        ]
+    )
+    return data
