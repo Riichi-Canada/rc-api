2beb041066317e1c45a47c0856b79d1edc19e20f
John-Johns0n <84598043+John-Johns0n@users.noreply.github.com>
2025-07-16 21:40:50 -0400

    Implement GET /players/{id}/event_results (WIP) Basic functionnality done, polishing not started

--- app/models.py
+++ app/models.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, Integer, String, Numeric
+from sqlalchemy import Column, Integer, String, Numeric, DateTime, Boolean
 from app.db.database import Base, engine
 
 
@@ -20,6 +20,24 @@ class Player(Base):
     player_score_2025_cycle = Column(Numeric)
     player_score_2028_cycle = Column(Numeric)
 
+class Event(Base):
+    __tablename__ = 'events'
+
+    id = Column(Integer, primary_key=True, index=True)
+    event_code = Column(String, index=True)
+    event_name = Column(String)
+    event_region = Column(Integer)
+    event_type = Column(Integer)
+    event_start_date = Column(DateTime)
+    event_end_date = Column(DateTime)
+    event_city = Column(String)
+    event_country = Column(String)
+    number_of_players = Column(Integer)
+    is_online = Column(Boolean)
+    event_ruleset = Column(Integer)
+    rule_modifications = Column(String)
+    event_notes = Column(String)
+
 class EventResult(Base):
     __tablename__ = 'event_results'
 
@@ -31,22 +49,50 @@ class EventResult(Base):
     placement = Column(Integer)
     score = Column(Numeric)
 
-# class PlayerScores2025(Base):
-#     __tablename__ = 'player_scores_2025_cycle'
-#
-#     id = Column(Integer, primary_key=True, index=True)
-#     player_id = Column(Integer)
-#     total_score = Column(Numeric)
-#     out_of_region_live = Column(Integer)
-#     other_live_1 = Column(Integer)
-#     other_live_2 = Column(Integer)
-#     any_event_1 = Column(Integer)
-#     any_event_2 = Column(Integer)
-#     tank_1 = Column(Integer)
-#     tank_2 = Column(Integer)
-#     tank_3 = Column(Integer)
-#     tank_4 = Column(Integer)
-#     tank_5 = Column(Integer)
+class EventScores2025(Base):
+    __tablename__ = 'event_scores_2025_cycle'
+
+    id = Column(Integer, primary_key=True, index=True)
+    result_id = Column(Integer, index=True)
+    main_score = Column(Numeric)
+    tank_score = Column(Numeric)
+
+class EventScores2028(Base):
+    __tablename__ = 'event_scores_2028_cycle'
+
+    id = Column(Integer, primary_key=True, index=True)
+    result_id = Column(Integer, index=True)
+    part_a = Column(Numeric)
+    part_b = Column(Numeric)
+
+class PlayerScores2025(Base):
+    __tablename__ = 'player_scores_2025_cycle'
+
+    id = Column(Integer, primary_key=True, index=True)
+    player_id = Column(Integer, index=True)
+    total_score = Column(Numeric)
+    out_of_region_live = Column(Integer)
+    other_live_1 = Column(Integer)
+    other_live_2 = Column(Integer)
+    any_event_1 = Column(Integer)
+    any_event_2 = Column(Integer)
+    tank_1 = Column(Integer)
+    tank_2 = Column(Integer)
+    tank_3 = Column(Integer)
+    tank_4 = Column(Integer)
+    tank_5 = Column(Integer)
+
+class PlayerScores2028(Base):
+    __tablename__ = 'player_scores_2028_cycle'
+
+    id = Column(Integer, primary_key=True, index=True)
+    player_id = Column(Integer, index=True)
+    total_score = Column(Numeric)
+    slot_1 = Column(Integer)
+    slot_2 = Column(Integer)
+    slot_3 = Column(Integer)
+    slot_4 = Column(Integer)
+    slot_5 = Column(Integer)
 
 
 def create_database():
--- app/routers/players.py
+++ app/routers/players.py
@@ -1,4 +1,5 @@
-from typing import Annotated
+import datetime as d
+from typing import Annotated, Any
 
 from fastapi import APIRouter, HTTPException, status, Depends, Query, Response
 from sqlalchemy import asc
@@ -8,12 +9,18 @@ from pydantic import BaseModel
 from app.db.database import get_db
 from app.models import Player as PlayerModel
 from app.models import EventResult as ResultModel
+from app.models import Event as EventModel
+from app.models import EventScores2025 as EventScores2025Model
+from app.models import EventScores2028 as EventScores2028Model
+from app.models import PlayerScores2025 as PlayerScores2025Model
+from app.models import PlayerScores2028 as PlayerScores2028Model
 from app.authentication import validate_api_key
 
 router = APIRouter(
     prefix='/api/v1/players'
 )
 
+# region PyDantic Models
 class Player(BaseModel):
     id: int
     first_name: str
@@ -42,6 +49,9 @@ class Results (BaseModel):
 #
 # class Scores2025(BaseModel):
 #     records: list[PlayerScores2025]
+
+# endregion PyDantic Models
+
 
 @router.get('')
 async def get_players(
@@ -96,15 +106,90 @@ async def get_player_results(
     player_id: int,
     db: Session = Depends(get_db),
     _: str = Depends(validate_api_key)
-) -> Results:
+) -> Any:
     results = db.query(ResultModel).filter(ResultModel.player_id == player_id).all()
 
     if results is None:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f'No results found for player with id {player_id}')
 
-    data = Results(
-        records=[
-            Result(event_name=str(result.event_id), placement=result.placement, score=result.score) for result in results
-        ]
-    )
-    return data
+    records = []
+
+    for result in results:
+        event = db.query(EventModel).filter(EventModel.id == result.event_id).first()
+
+        if event.event_start_date >= d.date(2022, 1, 1) and event.event_end_date <= d.date(2024, 12, 31):
+            event_rank_points = db.query(EventScores2025Model).filter(
+                EventScores2025Model.result_id == result.id).first()
+
+            ranking_points = {
+                'main_points': event_rank_points.main_score,
+                'tank': event_rank_points.tank_score
+            }
+
+            player_scores = db.query(PlayerScores2025Model).filter(
+                PlayerScores2025Model.player_id == result.player_id
+            ).first()
+
+            counts_for_relevant_cycle = (
+                result.id == player_scores.out_of_region_live or
+                result.id == player_scores.other_live_1 or
+                result.id == player_scores.other_live_2 or
+                result.id == player_scores.any_event_1 or
+                result.id == player_scores.any_event_2 or
+                result.id == player_scores.tank_1 or
+                result.id == player_scores.tank_2 or
+                result.id == player_scores.tank_3 or
+                result.id == player_scores.tank_4 or
+                result.id == player_scores.tank_5
+            )
+
+        elif event.event_start_date >= d.date(2025, 1, 1) and event.event_end_date <= d.date(2027, 12, 31):
+            event_rank_points = db.query(EventScores2028Model).filter(
+                EventScores2028Model.result_id == result.id
+            ).first()
+
+            ranking_points = {
+                'part_A': event_rank_points.part_a,
+                'part_B': event_rank_points.part_b
+            }
+
+            player_scores = db.query(PlayerScores2028Model).filter(
+                PlayerScores2028Model.player_id == result.player_id
+            ).first()
+
+            counts_for_relevant_cycle = (
+                    result.id == player_scores.slot_1 or
+                    result.id == player_scores.slot_2 or
+                    result.id == player_scores.slot_3 or
+                    result.id == player_scores.slot_4 or
+                    result.id == player_scores.slot_5
+            )
+
+        else:  # Event is from earlier than 2022, when Canada had no formalized ranking system
+            ranking_points = {}
+            counts_for_relevant_cycle = False
+
+        result_data = {
+            'event_details': {
+                'event_name': event.event_name,
+                'event_region': event.event_region,
+                'event_type': event.event_type,
+                'event_start_date': event.event_start_date,
+                'event_end_date': event.event_end_date,
+                'event_city': event.event_city,
+                'event_country': event.event_country,
+                'number_of_players': event.number_of_players,
+                'event_is_online': event.is_online,
+                'event_ruleset': event.event_ruleset
+            },
+            'result_details': {
+                'placement': result.placement,
+                'score': result.score,
+                'ranking_points': ranking_points,
+                'counts_for_relevant_cycle': counts_for_relevant_cycle
+            }
+        }
+
+        records.append(result_data)
+
+    return records
