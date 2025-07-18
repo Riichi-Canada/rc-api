c20188eca0c0638e48c022fff7496e2027195fe9
John-Johns0n <84598043+John-Johns0n@users.noreply.github.com>
2025-07-18 16:07:52 -0400

    Improve pagination syntax

--- app/pager.py
+++ app/pager.py
@@ -15,20 +15,22 @@ def paginate(query, route: str, page: int, per_page: int) -> dict[str, Any]:
     records = query.offset((page - 1) * per_page).limit(per_page).all()
 
     metadata = {
-        "page": page,
-        "per_page": per_page,
-        "page_count": page_count,
-        "total_count": total_count,
-        "links": [
-            {"self": f"/{route}?page={page}&per_page={per_page}"},
-            {"first": f"/{route}?page=1&per_page={per_page}"},
-            {"previous": f"/{route}?page={max(1, page - 1)}&per_page={per_page}"},
-            {"next": f"/{route}?page={min(page + 1, page_count)}&per_page={per_page}"},
-            {"last": f"/{route}?page={page_count}&per_page={per_page}"}
-        ]
+        'pagination': {
+            'page': page,
+            'per_page': per_page,
+            'page_count': page_count,
+            'total_count': total_count,
+            'links': [
+                {'self': f'/{route}?page={page}&per_page={per_page}'},
+                {'first': f'/{route}?page=1&per_page={per_page}'},
+                {'previous': f'/{route}?page={max(1, page - 1)}&per_page={per_page}'},
+                {'next': f'/{route}?page={min(page + 1, page_count)}&per_page={per_page}'},
+                {'last': f'/{route}?page={page_count}&per_page={per_page}'}
+            ]
+        }
     }
 
     return {
-        "metadata": metadata,
-        "records": records
+        'metadata': metadata,
+        'records': records
     }
